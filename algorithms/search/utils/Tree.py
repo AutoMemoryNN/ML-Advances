5efc61caa7b8c6e8e71cd1b97724beb537db28cb
KenjiroYamato <juanpablocamargo91@gmail.com>
2024-09-24 14:00:07 -0500

    feat(IAIA laboratory) - Tree implementation

---
+++ algorithms/search/utils/Tree.py
@@ -0,0 +1,81 @@
+class Tree:
+    def __init__(self, node: tuple[int, int]):
+        """
+        Crea un nuevo árbol con los datos proporcionados como raíz.
+
+        :param node: Los datos del árbol, representados como una tupla (x, y).
+        """
+        self.root = node
+        self.children = []
+        self.parent = None
+
+    def addChild(self, node: tuple[int, int], child: tuple[int, int]):
+        """
+        Busca el nodo dado en el árbol. Cuando se encuentra el nodo,
+        se agrega el 'child' como hijo del nodo.
+
+        :param node: El nodo a buscar.
+        :param child: El hijo a añadir.
+        """
+        if child == self.root:
+            return
+        if node == self.root:
+            new_child = Tree(child)
+            new_child.parent = self
+            self.children.append(new_child)
+        else:
+            for tree in self.children:
+                tree.addChild(node, child)
+
+    def getAllParents(self, leaf: tuple[int, int]):
+        """
+        Retorna todos los padres de la hoja dada.
+
+        :param leaf: La hoja a buscar.
+        :return: La lista de padres de la hoja.
+        """
+        current = self.findNode(leaf)
+        parents = [current]
+        while current.parent is not None:
+            parents.append(current.parent)
+            current = current.parent
+        return parents
+
+    def findNode(self, leaf: tuple[int, int]):
+        """
+        Busca el nodo dado en el árbol.
+
+        :param leaf: El nodo a buscar.
+        :return: El nodo si se encuentra, None en caso contrario.
+        """
+        if self.root == leaf:
+            return self
+        for tree in self.children:
+            found = tree.findNode(leaf)
+            if found is not None:
+                return found
+        return None
+
+    def isLeaf(self):
+        """
+        Verifica si el nodo es una hoja.
+
+        :return: True si es una hoja, False en caso contrario.
+        """
+        return len(self.children) == 0
+
+    def isRoot(self):
+        """
+        Verifica si el nodo es la raíz.
+
+        :return: True si es la raíz, False en caso contrario.
+        """
+        return self.parent is None
+
+    def __str__(self):
+        """
+        Representación en string del árbol.
+
+        :return: El string que representa los datos del nodo y sus hijos.
+        """
+        return f"{self.root} {self.children}"
